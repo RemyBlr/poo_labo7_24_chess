174cd75b80fd30cdb47bd6075e3d793b4213fd88
Cat-du-Fromage <florian.duruz@heig-vd.ch>
2024-12-12 13:32:56 +0100

    Update King.Java isValidMove

--- src/engine/piece/King.java
+++ src/engine/piece/King.java
@@ -14,4 +14,10 @@ public class King extends MovableOncePiece {
     public PieceType type() {
         return PieceType.KING;
     }
+
+    @Override
+    public boolean isValidMove(int fromX, int fromY, int toX, int toY) {
+        //samePosition deja pris en charge
+        return !(Math.abs(toX - fromX) > 1 || Math.abs(toY - fromY) > 1);
+    }
 }
