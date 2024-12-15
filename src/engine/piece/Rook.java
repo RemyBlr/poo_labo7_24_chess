5ce42b42332748205d21a1b9bd74c6a562b29e2d
Yoy017 <yoann.changanaqui@heig-vd.ch>
2024-12-15 14:20:22 +0100

    Rook first initiative

--- src/engine/piece/Rook.java
+++ src/engine/piece/Rook.java
@@ -2,6 +2,8 @@ package engine.piece;
 
 import chess.PieceType;
 import chess.PlayerColor;
+import engine.Board;
+import engine.Move;
 
 public class Rook extends MovableOncePiece {
 
@@ -14,14 +16,34 @@ public class Rook extends MovableOncePiece {
         return PieceType.ROOK;
     }
 
-    public boolean isValidMove(int fromX, int fromY, int toX, int toY) {
+    public boolean isValidMove(int fromX, int fromY, int toX, int toY, Board board, Move lastMove) {
 
-        // Cannot go if obstacles
-        for(int i = fromX, j = fromY; i < toX && toY < fromY; ++i, ++j) {
+        if(fromX != toX && fromY != toY) {
+            return false;
+        }
+
+        // Cannot go through obstacles
+        if(fromX == toX) {
+            int step = fromY < toY ? 1 : -1;
+            for(int i = fromY + step; i != toY; i += step) {
+                if(board.getPiece(fromX, i) != null) {
+                    return false;
+                }
+            }
+        } else {
+            int step = fromX < toX ? 1 : -1;
+            for(int i = fromX + step; i != toX; i += step) {
+                if(board.getPiece(i, fromY) != null) {
+                    return false;
+                }
+            }
+        }
+
+//        for(int i = fromX, j = fromY; i < toX && toY < fromY; ++i, ++j) {
 //            if( != null) {
 //
 //            }
-        }
+//        }
 
         return true;
     }
