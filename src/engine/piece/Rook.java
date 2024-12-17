a90cce61c43554e6c1ea1310aa1eac92ae50e953
Cat-du-Fromage <florian.duruz@heig-vd.ch>
2024-12-17 09:49:34 +0100

    Update Rook.java

--- src/engine/piece/Rook.java
+++ src/engine/piece/Rook.java
@@ -16,27 +16,21 @@ public class Rook extends MovableOncePiece {
         return PieceType.ROOK;
     }
 
-    private boolean isDiagonalMove(int fromX, int fromY, int toX, int toY) {
-        return Math.abs(toX - fromX) == Math.abs(toY - fromY);
+    private boolean isStraightMove(int fromX, int fromY, int toX, int toY) {
+        return fromX == toX || fromY == toY;
     }
 
     public boolean isValidMove(int fromX, int fromY, int toX, int toY, Board board, Move lastMove) {
         // Cannot go through obstacles
-        if(!isDiagonalMove(fromX, fromY, toX, toY)) {
-            return false;
-        }
+        if(!isStraightMove(fromX, fromY, toX, toY)) return false;
 
         // Check if there is a piece on the path the rook wants to go through
-        int incrX = 1, incrY = 1; // Déplacement diagonal haut droite
-        if(fromX > toX) incrX = -1;
-        if(fromY > toY) incrY = -1;
+        int incrX = Integer.compare(toX, fromX);
+        int incrY = Integer.compare(toY, fromY);
 
-        for(int i = fromX, j = fromY; i < toX && toY < fromY; i += incrX, j += incrY) {
-            if(board.getPiece(i, j) != null) {
-                return false;
-            }
+        for (int x = fromX + incrX, y = fromY + incrY; x != toX || y != toY; x += incrX, y += incrY) {
+            if (board.getPiece(x, y) != null) return false;
         }
-
         return true;
     }
 }
